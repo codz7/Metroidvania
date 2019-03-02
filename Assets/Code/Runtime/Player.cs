14d5e8d0eb972e13f9573fd237b180c6f51048b9
Olle Hagman <codz57@gmail.com>
2019-03-02 21:47:27 +0100

    basic player movement

---
+++ Assets/Code/Runtime/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D mRidgidbody;
+    public float mJumpHight;
+
+	void Start ()
+    {
+        mRidgidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mRidgidbody.AddForce(Vector2.up * mJumpHight, ForceMode2D.Impulse);
+        }
+    }
+}
