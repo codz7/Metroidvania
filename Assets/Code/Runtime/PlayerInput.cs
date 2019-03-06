47548e2afa9e78c3b1bd6eb17dacffc563b4c9ef
Olle Hagman <codz57@gmail.com>
2019-03-06 22:27:40 +0100

    Added double jump even thou its a bit wonky

--- Assets/Code/Runtime/PlayerInput.cs
+++ Assets/Code/Runtime/PlayerInput.cs
@@ -2,13 +2,28 @@
 
 public class PlayerInput : MonoBehaviour
 {
-
     public Rigidbody2D mRidgidbody;
+    public Transform feetPos;
 
     public float mJumpHight = 5.0f;
     public float mRunSpeed = 2.0f;
+
+    public bool mHasJumpedOnce = false;
+    public bool mHasJumpedTwice = false;
     
-	void Update ()
+
+    void FixedUpdate()
+    {
+        var hit = Physics2D.Raycast(feetPos.position, Vector2.down, 0.5f);
+        if (hit.collider != null)
+        {
+            Debug.Log("RAycast Hit!");
+            mHasJumpedOnce = false;
+            mHasJumpedTwice = false;
+        }
+    }
+
+    void Update ()
     {
         if (Input.GetAxis("Horizontal") > 0)
         {
@@ -22,7 +37,21 @@ public class PlayerInput : MonoBehaviour
         
         if (Input.GetButtonDown("Jump"))
         {
-            mRidgidbody.AddForce(Vector2.up * mJumpHight, ForceMode2D.Impulse);
+            if (mHasJumpedOnce == false)
+            {
+                mRidgidbody.AddForce(Vector2.up * mJumpHight, ForceMode2D.Impulse);
+                mHasJumpedOnce = true;
+            }
+            else if (mHasJumpedTwice == false)
+            {
+                mRidgidbody.AddForce(Vector2.up * mJumpHight * 2, ForceMode2D.Impulse);
+                mHasJumpedTwice = true;
+            }
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 0.5f);
     }
 }
